45d9f62342920a30210035fc137104fef7954930
PedrixzZDev <pedrixzz6@gmail.com>
2025-06-05 22:12:08 +0000

    test

--- src/main/java/org/featherwhisker/rendereng/mixins/RenderSystemMixin.java
+++ src/main/java/org/featherwhisker/rendereng/mixins/RenderSystemMixin.java
@@ -1,6 +1,6 @@
 package org.featherwhisker.rendereng.mixins;
 
-import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.opengl.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
