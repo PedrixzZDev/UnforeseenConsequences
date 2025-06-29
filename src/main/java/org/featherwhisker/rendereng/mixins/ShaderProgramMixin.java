a410311da28caff0ca7be30c1cbdf36bd7853858
PedrixzZDev <pedrixzz6@gmail.com>
2025-06-29 01:30:56 +0000

    test

--- src/main/java/org/featherwhisker/rendereng/mixins/ShaderProgramMixin.java
+++ src/main/java/org/featherwhisker/rendereng/mixins/ShaderProgramMixin.java
@@ -5,23 +5,22 @@ import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
 import java.util.List;
 
-import static com.mojang.blaze3d.systems.RenderSystem.glShaderSource;
+// Importação corrigida: A função agora vem da biblioteca LWJGL diretamente.
+import static org.lwjgl.opengl.GL20.glShaderSource;
 import static org.featherwhisker.rendereng.main.convertShader;
 import static org.featherwhisker.rendereng.main.shouldConvertShaders;
 
-// A classe alvo mudou de ShaderStage para ShaderProgram
 @Mixin(ShaderProgram.class)
 public class ShaderProgramMixin {
 
-    // A chamada para glShaderSource foi movida de GlStateManager para RenderSystem
+    // A classe alvo do redirect está correta, mas a forma de chamar o método precisa mudar.
+    // O Redirect ainda é necessário para interceptar o código, mas a chamada dentro dele será para a classe certa.
     @Redirect(
             method = "loadShader(Lnet/minecraft/client/gl/Shader;Ljava/lang/String;Ljava/io/InputStream;Ljava/lang/String;)V",
             at = @At(
                     value = "INVOKE",
-                    // O alvo da chamada agora é RenderSystem
                     target = "Lcom/mojang/blaze3d/systems/RenderSystem;glShaderSource(ILjava/util/List;)V"
             )
     )
@@ -32,7 +31,7 @@ public class ShaderProgramMixin {
                 strings.set(i, convertShader(originalShader, i));
             }
         }
-        // Chama o método original (que agora é RenderSystem.glShaderSource) com a lista modificada
+        // Chamada corrigida: Usando o método estático importado de org.lwjgl.opengl.GL20
         glShaderSource(shader, strings);
     }
 }
--- src/main/java/org/featherwhisker/rendereng/mixins/WindowMixin.java
+++ src/main/java/org/featherwhisker/rendereng/mixins/WindowMixin.java
@@ -1,23 +1,27 @@
 package org.featherwhisker.rendereng.mixins;
+
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.MinecraftVersion;
-import org.spongepowered.asm.mixin.Mixin;
 import net.minecraft.client.util.Window;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import static org.featherwhisker.rendereng.main.log;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL30.*;
+// GL30 não é mais usado aqui, podemos remover se não for necessário em outro lugar
+// import static org.lwjgl.opengl.GL30.*; 
+
 @Environment(EnvType.CLIENT)
 @Mixin(Window.class)
 public class WindowMixin {
     // Inject our function right before the window is created
-    @Inject(method = "<init>", at = @At(value = "INVOKE",remap = false, target = "org/lwjgl/glfw/GLFW.glfwCreateWindow (IILjava/lang/CharSequence;JJ)J", shift = At.Shift.BEFORE))
+    @Inject(method = "<init>", at = @At(value = "INVOKE",remap = false, target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J", shift = At.Shift.BEFORE))
     public void injected(CallbackInfo ci) {
-        //Get frame name
-        String frameName = "Minecraft "+MinecraftVersion.create().getName();
+        // CORREÇÃO: Usar .name() em vez de .getName()
+        String frameName = "Minecraft " + MinecraftVersion.create().name();
 
         // Throw out whatever vanilla tells GLFW
         glfwDefaultWindowHints();
@@ -28,33 +32,36 @@ public class WindowMixin {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
         // Rendering related things
-        glfwWindowHint(GLFW_REFRESH_RATE, GL_DONT_CARE);
-        glfwWindowHint(GLFW_SAMPLES, GL_NONE);
+        glfwWindowHint(GLFW_REFRESH_RATE, GLFW_DONT_CARE);
+        // A constante GL_NONE não existe em GLFW, o valor correto é 0 ou simplesmente não definir. 
+        // Usaremos 0 para clareza, pois é o valor padrão para desabilitar MSAA.
+        glfwWindowHint(GLFW_SAMPLES, 0);
 
         // Misc OpenGL hints
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
         glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
-        glfwWindowHint(GLFW_CONTEXT_NO_ERROR, GL_TRUE); //more performance + not having to rewrite texture attaching
+        glfwWindowHint(GLFW_CONTEXT_NO_ERROR, GLFW_TRUE); //more performance + not having to rewrite texture attaching
 
         //Platform Specific
         try {
             String str = "Running on: ";
-            if (glfwGetPlatform() == GLFW_PLATFORM_X11) {
+            int platform = glfwGetPlatform();
+            if (platform == GLFW_PLATFORM_X11) {
                 str = str + "*nix with X11";
                 glfwWindowHintString(GLFW_X11_CLASS_NAME, "Minecraft");
                 glfwWindowHintString(GLFW_X11_INSTANCE_NAME, frameName);
-            } else if (glfwGetPlatform() == GLFW_PLATFORM_WAYLAND) {
+            } else if (platform == GLFW_PLATFORM_WAYLAND) {
                 str = str + "*nix with Wayland";
-            } else if (glfwGetPlatform() == GLFW_PLATFORM_COCOA) {
+            } else if (platform == GLFW_PLATFORM_COCOA) {
                 str = str + "macOS";
                 glfwWindowHintString(GLFW_COCOA_FRAME_NAME, frameName);
-            } else if (glfwGetPlatform() == GLFW_PLATFORM_WIN32) {
+            } else if (platform == GLFW_PLATFORM_WIN32) {
                 str = str + "Windows";
             } else {
                 str = str +  "Unknown platform";
             }
             log.info(str);
-            if (str != "Running on: Windows"){
+            if (platform != GLFW_PLATFORM_WIN32){
                 log.info("There are known issues running on non-windows platforms.");
             }
         } catch(Exception ignored) {
